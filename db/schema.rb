f969333a56ee8750b8dd24ffc9a07816690c92e7
Michael Spalding <spalding.michael@gmail.com>
2015-09-12 03:02:08 +0000

    Modified devisefor stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909040450) do
+ActiveRecord::Schema.define(version: 20150912025744) do
 
   create_table "contacts", force: true do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150909040450) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
